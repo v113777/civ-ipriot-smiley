9566568133eeb660088d23ab900101f4ea411e14
v113777 <128450533+v113777@users.noreply.github.com>
2023-06-10 22:25:38 +0800

    Addressed requirements of the porfolio

---
+++ angry.py
@@ -0,0 +1,33 @@
+from smiley import Smiley
+
+
+class Sad(Smiley):
+    def __init__(self, complexion = Smiley.BLUE):
+        super().__init__(complexion)
+
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+        """
+        Method that draws the mouth on the standard faceless smiley.
+        """
+        mouth = [49, 54, 42, 43, 44, 45]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        """
+        Method that draws the eyes (open or closed) on the standard smiley.
+        :param wide_open: True if eyes opened, False otherwise
+        """
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+
+    def blink(self, delay=0.25):
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
